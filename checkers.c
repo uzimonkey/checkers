9e818d3b6ad3e73e5c3e346317d61aa081deb0e6
UziMonkey <uzimonkey@gmail.com>
2019-04-25 19:33:06 -0400

    DRYed up board_display

--- checkers.c
+++ checkers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdarg.h>
 
 enum { EMPTY, BLACK, WHITE, BLACK_KING, WHITE_KING };
 char pieces[] = {' ', 'b', 'w', 'B', 'W' };
@@ -18,31 +19,37 @@ void board_init() {
   }
 }
 
-void board_display(int indent) {
-  for(int i = 0; i < indent; i++) printf(" ");
-  printf("┌─┬─┬─┬─┬─┬─┬─┬─┐\n");
+void indent(int identation) {
+  while(identation--) printf(" ");
+}
+
+void printf_indented(int indentation, const char *fmt, ...) {
+  indent(indentation);
+
+  va_list args;
+  va_start(args, fmt);
+  vprintf(fmt, args);
+}
+
+void board_display(int indentation) {
+  printf_indented(indentation+2, " 1 2 3 4 5 6 7 8\n");
+  printf_indented(indentation+2, "┌─┬─┬─┬─┬─┬─┬─┬─┐\n");
 
   for(int y = 0; y < 8; y++) {
-    for(int i = 0; i < indent; i++) printf(" ");
-    printf("│");
+    printf_indented(indentation, "%1d │", y+1);
 
-    for(int x = 0; x < 8; x++) {
+    for(int x = 0; x < 8; x++)
       printf("%c│", pieces[board[y][x]]);
-    }
 
     printf("\n");
-    if(y != 7) {
-      for(int i = 0; i < indent; i++) printf(" ");
-      printf("├─┼─┼─┼─┼─┼─┼─┼─┤\n");
-    }
+    if(y != 7)
+      printf_indented(indentation+2, "├─┼─┼─┼─┼─┼─┼─┼─┤\n");
+    else
+      printf_indented(indentation+2, "└─┴─┴─┴─┴─┴─┴─┴─┘\n");
   }
-
-  for(int i = 0; i < indent; i++) printf(" ");
-  printf("└─┴─┴─┴─┴─┴─┴─┴─┘\n");
 }
 
 int main(int argc, char *argv[]) {
   board_init();
   board_display(8);
 }
-
