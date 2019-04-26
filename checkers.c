2167a973d8f0de0193a5661ed92d1db288012403
UziMonkey <uzimonkey@gmail.com>
2019-04-26 04:44:15 -0400

    Better representation of the players helped me clean up a lot of conditionals

--- checkers.c
+++ checkers.c
@@ -3,21 +3,35 @@
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-enum { EMPTY, BLACK, WHITE, BLACK_KING, WHITE_KING };
-char pieces[] = {' ', 'b', 'w', 'B', 'W' };
-int board[8][8];
+char board[8][8];
+
+struct player {
+  const char *name;
+  char piece;
+  int king_row;
+  int dir;
+  int score;
+} players[] = {
+  {.name="White", .piece='w', .king_row=7, .dir=1},
+  {.name="Black", .piece='b', .king_row=0, .dir=-1}
+};
+
+#define NUM_PLAYERS (sizeof(players)/sizeof(players[0]))
+  
 
 // Initialize board and place starting pieces
 void board_init() {
-  memset(&board, EMPTY, 8*8);
+  memset(&board, ' ', 8*8);
 
   for(int y = 0; y < 8; y++) {
     for(int x = 0; x < 8; x++) {
       if(y%2 == x%2) continue; // Skip red spaces
 
-      if(y < 3) board[y][x] = WHITE;
-      else if(y >= 5) board[y][x] = BLACK;
+      // TODO
+      if(y < 3) board[y][x] = players[0].piece;
+      else if(y >= 5) board[y][x] = players[1].piece;
     }
   }
 }
@@ -40,7 +54,7 @@ void board_display(int indentation) {
     printf_indented(indentation, "%1d │", y+1);
 
     for(int x = 0; x < 8; x++)
-      printf("%c│", pieces[board[y][x]]);
+      printf("%c│", board[y][x]);
 
     printf("\n");
     if(y != 7)
@@ -50,39 +64,40 @@ void board_display(int indentation) {
   }
 }
 
+// Return sign of x (-1, 0 or 1)
+int sign(int x) {
+  return -(x<0) + (x>0);
+}
+
 // Determine if a move is valid
-bool valid_move(int x1, int y1, int x2, int y2, int turn) {
+bool valid_move(int x1, int y1, int x2, int y2, struct player *pl) {
+  // Off the board
   if(x1 < 0 || x1 > 7 || y1 < 0 || y1 > 7) return false;
   if(x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7) return false;
-  if(abs(x1-x2) != abs(y1-y2)) return false;
-  if(board[y1][x1] == EMPTY || board[y1][x1] != turn) return false;
 
+  // Not a diagonal
+  if(abs(x1-x2) != abs(y1-y2)) return false;
+
+  // Space not empty
+  if(board[y2][x2] != ' ') return false;
+
+  char piece = board[y1][x1];
   int distance = abs(x1-x2);
+
+  // Wrong color piece
+  if(piece == ' ' || tolower(piece) != pl->piece)
+    return false;
+
+  // Correct distance
   if(distance != 1 && distance != 2) return false;
 
-  int piece = board[y1][x1];
-  if(piece == WHITE && y2 < y1) return false;
-  if(piece == BLACK && y2 > y1) return false;
+  // Correct direction
+  if(piece != toupper(pl->piece) && sign(y2-y1) != pl->dir)
+    return false;
 
-  if(distance == 2) {
-    if(
-        piece == WHITE &&
-        board[y2][x2] != EMPTY &&
-        board[(y1+y2)/2][(x1+x2)/2] != BLACK
-      ) {
-      return false;
-    }
-
-    if(
-        piece == BLACK &&
-        board[y2][x2] != EMPTY &&
-        board[(y1+y2)/2][(x1+x2)/2] != WHITE
-      ) {
-      return false;
-    }
-  } else {
-    if(board[y2][x2] != EMPTY) return false;
-  }
+  // Jump piece
+  if(distance == 2 && tolower(board[(y1+y2)/2][(x1+x2)/2]) == pl->piece)
+    return false;
 
   return true;
 }
@@ -90,20 +105,18 @@ bool valid_move(int x1, int y1, int x2, int y2, int turn) {
 // Perform a move, capture piece if jumped
 void move(int x1, int y1, int x2, int y2) {
   board[y2][x2] = board[y1][x1];
-  board[y1][x1] = EMPTY;
+  board[y1][x1] = ' ';
   if(abs(x1-x2) == 2)
-    board[(y2+y1)/2][(x2+x1)/2] = EMPTY;
+    board[(y2+y1)/2][(x2+x1)/2] = ' ';
 }
 
 // Count the pieces each player has on board
-void board_score(int *white_score, int *black_score) {
-  *white_score = *black_score = 0;
-
+void update_player_score() {
   for(int y = 0; y < 7; y++) {
     for(int x = 0; x < y; x++) {
       int p = board[y][x];
-      *white_score += p == WHITE || p == WHITE_KING;
-      *black_score += p == BLACK || p == BLACK_KING;
+      for(int i = 0; i < NUM_PLAYERS; i++)
+        players[i].score += tolower(p) == players[i].piece;
     }
   }
 }
@@ -144,12 +157,14 @@ char *getinput(FILE *f) {
 // Play a game, return when there's a winner
 void play_game() {
   board_init();
+  for(int i = 0; i < NUM_PLAYERS; i++)
+    players[i].score = 0;
 
-  int turn = WHITE;
+  int turn = 0;
   while(1) {
     board_display(4);
 
-    printf("%s, it is your turn.\n", turn == WHITE ? "White" : "Black");
+    printf("%s, it is your turn.\n", players[turn].name);
     printf("Enter move: ");
     char *line = getinput(stdin);
     int x1, x2, y1, y2;
@@ -164,28 +179,23 @@ void play_game() {
     x2--;
     y2--;
 
-    if(!valid_move(x1, y1, x2, y2, turn)) {
+    if(!valid_move(x1, y1, x2, y2, &players[turn])) {
       printf("Invalid move\n");
       continue;
     }
 
     move(x1, y1, x2, y2);
-    if(turn == WHITE && y2 == 7)
-      board[y2][x2] = WHITE_KING;
-    else if(turn == BLACK && y2 == 0)
-      board[y2][x2] = BLACK_KING;
+    if(y2 == players[turn].king_row)
+      board[y2][x2] = toupper(players[turn].piece);
 
-    int white_score, black_score;
-    board_score(&white_score, &black_score);
-    if(white_score == 0) {
-      printf("Black wins!\n");
-      return;
-    } else if(black_score == 0) {
-      printf("White wins!\n");
-      return;
-    }
+    update_player_score();
+    for(int i = 0; i < NUM_PLAYERS; i++)
+      if(players[i].score == 0) {
+        printf("%s loses!\n", players[i].name);
+        return;
+      }
 
-    turn = turn == WHITE ? BLACK : WHITE;
+    turn = (turn+1) % NUM_PLAYERS;
   }
 }
 
