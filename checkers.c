89968af5fd48807fa4ecd64461ce31d06c46f445
UziMonkey <uzimonkey@gmail.com>
2019-04-26 04:01:38 -0400

    Games can be played

--- checkers.c
+++ checkers.c
@@ -51,11 +51,11 @@ void board_display(int indentation) {
   }
 }
 
-bool valid_move(int x1, int y1, int x2, int y2) {
+bool valid_move(int x1, int y1, int x2, int y2, int turn) {
   if(x1 < 0 || x1 > 7 || y1 < 0 || y1 > 7) return false;
   if(x2 < 0 || x2 > 7 || y2 < 0 || y2 > 7) return false;
   if(abs(x1-x2) != abs(y1-y2)) return false;
-  if(board[y1][x1] == EMPTY) return false;
+  if(board[y1][x1] == EMPTY || board[y1][x1] != turn) return false;
 
   int distance = abs(x1-x2);
   if(distance != 1 && distance != 2) return false;
@@ -94,6 +94,18 @@ void move(int x1, int y1, int x2, int y2) {
     board[(y2+y1)/2][(x2+x1)/2] = EMPTY;
 }
 
+void board_score(int *white_score, int *black_score) {
+  *white_score = *black_score = 0;
+
+  for(int y = 0; y < 7; y++) {
+    for(int x = 0; x < y; x++) {
+      int p = board[y][x];
+      *white_score += p == WHITE || p == WHITE_KING;
+      *black_score += p == BLACK || p == BLACK_KING;
+    }
+  }
+}
+
 char *getinput(FILE *f) {
   static char *buf = NULL;
   static int buflen = 0;
@@ -126,28 +138,49 @@ char *getinput(FILE *f) {
   return buf;
 }
 
-int main(int argc, char *argv[]) {
+void play_game() {
   board_init();
 
+  int turn = WHITE;
   while(1) {
-    board_display(8);
-    printf("> ");
+    board_display(4);
 
-    int x1, x2, y1, y2;
+    printf("%s, it is your turn.\n", turn == WHITE ? "White" : "Black");
+    printf("Enter move: ");
     char *line = getinput(stdin);
-    if(sscanf(line, "%d,%d %d,%d", &x1, &y1, &x2, &y2) != 4)
+    int x1, x2, y1, y2;
+
+    if(sscanf(line, "%d,%d %d,%d", &x1, &y1, &x2, &y2) != 4) {
+      printf("Invalid move.\n");
       continue;
+    }
 
     x1--;
     y1--;
     x2--;
     y2--;
 
-    if(!valid_move(x1, y1, x2, y2)) {
+    if(!valid_move(x1, y1, x2, y2, turn)) {
       printf("Invalid move\n");
       continue;
     }
 
-    move(x1, y1, x2, y2);    
+    move(x1, y1, x2, y2);
+
+    int white_score, black_score;
+    board_score(&white_score, &black_score);
+    if(white_score == 0) {
+      printf("Black wins!\n");
+      return;
+    } else if(black_score == 0) {
+      printf("White wins!\n");
+      return;
+    }
+
+    turn = turn == WHITE ? BLACK : WHITE;
   }
+}
+
+int main(int argc, char *argv[]) {
+  play_game();
 }
